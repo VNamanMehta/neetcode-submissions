ee8e7c3598f468a22718a003ad3c4f96eb95205b
V Naman Mehta <138012631+VNamanMehta@users.noreply.github.com>
2026-07-30 22:36:03 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # return sorted(s) == sorted(t)
+        # c1 = {}
+        # c2 = {}
+
+        # if len(s) != len(t):
+        #      return False
+
+        # for i in s:
+        #     c1[i] = c1.get(i, 0) + 1
+        
+        # for i in t:
+        #     c2[i] = c2.get(i, 0) + 1
+        
+        # for i in c1:
+        #     if i not in c2 or c2[i] != c1[i]:
+        #         return False
+        # return True
+
+        if len(s) != len(t):
+            return False
+
+        lookup={}
+
+        for i in s:
+            lookup[i] = lookup.get(i,0)+1
+
+        for i in t:
+            if i not in lookup:
+                return False
+
+            lookup[i]-=1
+
+        return all(i==0 for i in lookup.values())    
